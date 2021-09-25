a518475b32bd91aee4350b546cdcd45c70d727ca
zadigg <62830716+zadigg@users.noreply.github.com>
2021-09-25 18:43:23 +0300

    last

--- components/SmallCards.js
+++ components/SmallCards.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-
-function SmallCards({ key, img, location, distance }) {
+// Thank You
+function SmallCards({ img, location, distance }) {
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
       {/* left */}
