ba2f526d4e0f1ddc4605c4b50b1007f89a079498
zadigg <62830716+zadigg@users.noreply.github.com>
2021-09-27 15:11:48 +0300

    day4 final

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
-  env: {
-    mapbox_key:
-      "pk.eyJ1IjoiemFkaWczNiIsImEiOiJja3UyZ2RxeTEwdWtvMnZvMWJlMnZiaHF4In0.kQFQtURPCEDvcEo1xdTy-w",
-  },
 };
